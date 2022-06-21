5f90bc6fd2a5776aa44776cff9e19a4b821d99a1
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2022-06-21 21:06:58 +0200

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by JC on 21/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
